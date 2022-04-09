6cd31b92948836bcdedb650d410e87219bdfd709
“bouzy” <bouzy@users.noreply.github.com>
2022-04-09 14:35:17 -0400

    Basic installation

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Basin installation
+
+sudo docker run -d \
+  --name=unifi-controller \
+  --privileged \
+  --restart=unless-stopped \
+  -v /home/eldon/docker/unifi:/config \
+  --network=host \
+  lscr.io/linuxserver/unifi-controller
