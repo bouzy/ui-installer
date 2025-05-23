2053169c6f555a123fd05c53eaf5ee2a238a494a
bouzy <bouzy@users.noreply.github.com>
2025-05-23 01:07:09 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 
-unifi_installer(){
-  rm -fr /opt/docker/unifi-controller
-  rm -fr /opt/docker/unifi-controller-db
-  rm -fr /opt/docker-compose/unifi-controller
+docker_directory="/opt/docker/"
+docker_compose_direcrtory="/opt/docker-compose"
 
-  sudo mv unifi-controller /opt/docker-compose
+docker_unifi_directory="/opt/docker/unifi-controller"
+docker_unifi_db_directory="/opt/docker/unifi-controller-db"
+docker_compose_unifi_directory="/opt/docker-compose/unifi-controller"
 
-  sudo /opt/docker-compose/unifi-controller/docker compose up
-}
+if [ -d "$docker_directory" ]; then
+  echo $docker_directory
+else
+  sudo mkdir $docker_directory
+fi
 
-unifi_installer
+if [ -d "$docker_compose_directory" ]; then
+  echo $docker_compose_direcrtory
+else
+  sudo mkdir $docker_compose_direcrtory
+fi
+
+if [ -d "$docker_unifi_directory" ]; then
+  rm -fr $docker_unifi_directory
+fi
+
+if [ -d "$docker_unifi_db_directory" ]; then
+  rm -fr $docker_unifi_db_directory
+fi
+
+if [ -d "$docker_compose_unifi_directory" ]; then
+  rm -fr $docker_compose_unifi_directory
+fi
+
+sudo cp unifi-controller $docker_compose_direcrtory
+sudo "$docker_compose_unifi_directory/docker compose up"
